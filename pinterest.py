023dbb337a539184922bd53aac20afb325d1d40e
smileqoo <105653195+smileqoo@users.noreply.github.com>
2022-12-04 01:23:53 +0800

    Update pinterest.py

--- pinterest.py
+++ pinterest.py
@@ -127,12 +127,12 @@ def pin_search(keyword,num):
     #num = int(input('輸入圖片數量:'))
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+   # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless") #無頭模式
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
+   # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome('.\chromedriver')
 
     driver.get(URL+keyword)
 
